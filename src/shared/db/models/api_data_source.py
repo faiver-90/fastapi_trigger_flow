17cb9b16512996996a4c0f80d50aa29f7aece478
faiver90 <faiver90@gmail.com>
2025-09-29 15:53:03 +0300

    change id type

--- src/shared/db/models/api_data_source.py
+++ src/shared/db/models/api_data_source.py
@@ -8,9 +8,9 @@ from src.shared.db.base import Base
 class Sources(Base):
     __tablename__ = "sources"
 
-    id: Mapped[int] = mapped_column(String(255), primary_key=True)
-    user_id: Mapped[int] = mapped_column(String(255), nullable=False)
-    source_type_id: Mapped[int] = mapped_column(String(255), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    source_type_id: Mapped[int] = mapped_column(Integer, nullable=False)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     config: Mapped[dict] = mapped_column(JSONB, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
